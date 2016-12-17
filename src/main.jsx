11d4d7b4d7705d80f58ac52397208f5a55373470
Kaloian Manassiev <kaloianm@gmail.com>
2016-12-17 12:18:55 -0500

    Update the favicon and cleanup unused apple images

--- src/main.jsx
+++ src/main.jsx
@@ -204,6 +204,12 @@ const websiteContent = (
 
     <div style={styles.whiteSpace}></div>
 
+    <div>
+        Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from 
+        <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by 
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+    </div>
+
   </div>
 );      
 
